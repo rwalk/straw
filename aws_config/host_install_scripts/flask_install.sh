19f36b09333028430b9f55abd4ff148fb781c563
Ryan Walker <ryan@ryanwalker.us>
2015-09-19 12:43:25 -0700

    Base for webserver frontend

--- aws_config/host_install_scripts/flask_install.sh
+++ aws_config/host_install_scripts/flask_install.sh
@@ -4,23 +4,7 @@
 ##########################################################
 
 # python3 discouraged: http://flask.pocoo.org/docs/0.10/python3/
-apt-get -y update
-apt-get install -y python-pip python-dev build-essential
-pip install flask
+sudo apt-get -y update
+sudo apt-get install -y python-pip python-dev build-essential
+sudo pip install flask
 
-# directories for content
-cd /home/ubuntu/
-mkdir app
-mkdir app/static
-mkdir app/templates
-mkdir tmp
-
-# initialize
-touch app/__init__.py
-echo "from flask import Flask" >> app/__init__.py
-echo "app = Flask(__name__)" >> app/__init__.py
-echo "from app import views" >> app/__init__.py
-
-# change owner to ubuntu user
-chown -R ubuntu:ubuntu app
-chown -R ubuntu:ubuntu tmp
---
+++ src/frontend/app/views.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
