b49a92ec6418b17908f52cdfbaff2d723b781aff
Ryan Walker <ryan@ryanwalker.us>
2015-09-25 08:12:06 -0700

    javascript template for dynamically updating result page with matching tweets

--- src/frontend/app/views.py
+++ src/frontend/app/views.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 
+from flask import render_template, request, render_template, jsonify
+from time import sleep
 from app import app
-from flask import render_template
+import json
+
+
+@app.route('/_fetch_messages')
+def fetch_messages():
+    return jsonify(result=app.disp)
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
--- src/frontend/run.py
+++ src/frontend/run.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
-
+from time import sleep
 from app import app
+import thread
 
 if __name__=="__main__":
+
+    # Move messages to display box in background
+
+    # other setup tasks
+    
+    app.disp = []
+
+    # Define a function for the thread
+    def load_msgs( threadName, app, msgs ):
+        while len(msgs)>0:
+            sleep(5)
+            app.disp.append(msgs.pop())
+        msgs = list("These are my messages")
+        app.disp=[]
+
+    msgs = list("These are my messages")
+    thread.start_new_thread( load_msgs, ("Thread-1", app, msgs) )
     app.run(host='0.0.0.0', debug = True)
+
