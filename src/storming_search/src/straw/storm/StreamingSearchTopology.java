451ef100a619692f429e439122ccf2c2fdd85ccc
Ryan Walker <ryan@ryanwalker.us>
2015-10-01 22:09:21 -0700

    topology config

--- src/storming_search/src/straw/storm/StreamingSearchTopology.java
+++ src/storming_search/src/straw/storm/StreamingSearchTopology.java
@@ -114,14 +114,14 @@ public class StreamingSearchTopology {
     // topology definition
     TopologyBuilder builder = new TopologyBuilder();
     builder.setSpout("query-spout", new KafkaSpout(query_spout_config), 1);
-    builder.setSpout("document-spout", new KafkaSpout(document_spout_config), 4);
-    builder.setBolt("search-bolt", new SearchBolt(), 4)
+    builder.setSpout("document-spout", new KafkaSpout(document_spout_config), 3);
+    builder.setBolt("search-bolt", new SearchBolt(), 3)
     	.allGrouping("query-spout")
     	.shuffleGrouping("document-spout");
     	
     // topology submission
     if (args != null && args.length > 0) {
-      config.setNumWorkers(4);
+      config.setNumWorkers(2);
       StormSubmitter.submitTopologyWithProgressBar(args[0], config, builder.createTopology());
     }
     else {
