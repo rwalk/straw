dddd1bfd2fe066283c4f343582a43fd5ca31276f
Ubuntu <ubuntu@ip-10-0-0-15.us-west-2.compute.internal>
2015-10-03 23:12:43 +0000

    Config wrong in submit script for luwak

--- src/luwak_search/submit_topology.sh
+++ src/luwak_search/submit_topology.sh
@@ -3,5 +3,5 @@ export STRAW_CONFIG=`pwd`/../../config/config.properties
 /usr/local/storm/bin/storm kill "prod-topology"
 /usr/local/storm/bin/storm deactivate "prod-topology"
 echo "USING CONFIG FILE: $STRAW_CONFIG"
-/usr/local/storm/bin/storm jar target/storming-search-0.0.1.jar straw.storm.StreamingSearchTopology "prod-topology"
+/usr/local/storm/bin/storm jar target/storming-luwak-search-0.0.1.jar straw.storm.LuwakSearchTopology "prod-topology"
 
