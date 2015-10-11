fecf483189bf75cbd680ea72d53e952761210806
Ubuntu <ubuntu@ip-10-0-0-6.us-west-2.compute.internal>
2015-10-11 18:00:15 +0000

    Only need kill in submit script

--- src/luwak_search/submit_topology.sh
+++ src/luwak_search/submit_topology.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export STRAW_CONFIG=`pwd`/../../config/config.properties
 /usr/local/storm/bin/storm kill "prod-topology"
-/usr/local/storm/bin/storm deactivate "prod-topology"
 echo "USING CONFIG FILE: $STRAW_CONFIG"
+sleep 30
 /usr/local/storm/bin/storm jar target/storming-luwak-search-0.0.1.jar straw.storm.LuwakSearchTopology "prod-topology"
 
