342138315981f55e4d61240645af705b628d8eca
Ryan Walker <ryan@ryanwalker.us>
2015-11-03 20:14:08 -0800

    Unpack data for demo mode

--- util/stage_demo_mode.sh
+++ util/stage_demo_mode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# unpack local data
+( cd ../data/ &&
+gunzip tweets.big.sample.gz -f -k
+)
+
+
 # start kafka
 (cd ../src/kafka_stream_eater/third_party/kafka-docker-master && docker-compose stop && docker-compose rm && docker-compose up -d --force-recreate)
 
